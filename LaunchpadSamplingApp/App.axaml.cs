e43b8c5251179b5d8906cd7e3e2ea7b997b9b4cd
Jay Feather2000 <thegals4321@gmail.com>
2023-09-04 03:05:28 -0500

    a

--- LaunchpadSamplingApp/App.axaml.cs
+++ LaunchpadSamplingApp/App.axaml.cs
@@ -2,7 +2,7 @@
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-
+using Avalonia.Themes.Fluent;
 using LaunchpadSamplingApp.ViewModels;
 using LaunchpadSamplingApp.Views;
 
---
+++ LaunchpadSamplingApp/Components/Launchpad.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace LaunchpadSamplingApp.Components
+{
+    public class Launchpad : TemplatedControl
+    {
+    }
+}
