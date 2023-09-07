e9c9aea5216b536cf31ff1184e129e9a6fffc948
Jay <thegals4321@gmail.com>
2023-09-06 21:50:45 -0500

    a

--- LaunchpadSamplingApp/Components/LaunchpadButton.axaml.cs
+++ LaunchpadSamplingApp/Components/LaunchpadButton.axaml.cs
@@ -1,11 +1,12 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Metadata;
-using Avalonia.Controls.Primitives;
 
 namespace LaunchpadSamplingApp.Components
 {
-    public class LaunchpadButton : TemplatedControl
+    public partial class LaunchpadButton : UserControl
     {
+        public LaunchpadButton()
+        {
+            InitializeComponent();
+        }
     }
 }
