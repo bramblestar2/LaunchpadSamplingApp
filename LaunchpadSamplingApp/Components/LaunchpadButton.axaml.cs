2497da43218bcbfd3b6e4b7a538776184ca7f8b9
Jay <thegals4321@gmail.com>
2023-09-08 00:24:35 -0500

    a

--- LaunchpadSamplingApp/Components/LaunchpadButton.axaml.cs
+++ LaunchpadSamplingApp/Components/LaunchpadButton.axaml.cs
@@ -1,12 +1,107 @@
+using Avalonia.Animation;
+using Avalonia.Animation.Easings;
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml.Converters;
+using Avalonia.Media;
+using Avalonia.Styling;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace LaunchpadSamplingApp.Components
 {
+    [PseudoClasses(":onPointerEnter", "onPointerLeave", "onPointerPressed", "onPointerReleased")]
     public partial class LaunchpadButton : UserControl
     {
+        private NAudio.Wave.WaveFileReader? _wave = null;
+        private NAudio.Wave.DirectSoundOut? _audioOut = null;
+
+
+        public readonly SolidColorBrush primaryBrush = new SolidColorBrush(Color.FromRgb(0x50, 0x50, 0x50));
+        public readonly SolidColorBrush hoveredBrush = new SolidColorBrush(Color.FromRgb(0x40, 0x40, 0x40));
+        public readonly SolidColorBrush pressedBrush = new SolidColorBrush(Color.FromRgb(0x20, 0x20, 0x20));
+
+
         public LaunchpadButton()
         {
             InitializeComponent();
+
+            //PseudoClasses.Add(":onhover");
         }
+
+
+        public void Load(string filepath)
+        {
+            if (File.Exists(filepath))
+            {
+                Unload(); //Unload possible audio
+
+                _wave = new NAudio.Wave.WaveFileReader(filepath);
+                _audioOut = new NAudio.Wave.DirectSoundOut();
+                _audioOut.Init(_wave);
+            }
+        }
+
+
+        private void Unload()
+        {
+            if (_audioOut != null && _wave != null)
+            {
+                _wave.Dispose();
+                _audioOut.Dispose();
+
+                _wave = null;
+                _audioOut = null;
+            }
+        }
+
+
+
+        #region Pointer Enter/Exit Events
+
+        protected override void OnPointerEntered(PointerEventArgs e)
+        {
+            base.OnPointerEntered(e);
+            
+            PseudoClasses.Set(":onPointerEnter", true);
+            PseudoClasses.Set(":onPointerLeave", false);
+        }
+
+        protected override void OnPointerExited(PointerEventArgs e)
+        {
+            base.OnPointerExited(e);
+
+            PseudoClasses.Set(":onPointerLeave", true);
+            PseudoClasses.Set(":onPointerEnter", false);
+        }
+
+        #endregion
+
+        #region Pointer Press/Release Events
+
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            PseudoClasses.Set(":onPointerPressed", true);
+            PseudoClasses.Set(":onPointerReleased", false);
+        }
+
+        protected override void OnPointerReleased(PointerReleasedEventArgs e)
+        {
+            base.OnPointerReleased(e);
+
+            PseudoClasses.Set(":onPointerReleased", true);
+            PseudoClasses.Set(":onPointerPressed", false);
+        }
+
+        #endregion
     }
+
 }
